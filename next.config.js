047998d5f22b2a9af89b5897ff979dd32806ba30
Rafał Kowalski <kontakt@kowalskirafal.pl>
2022-10-04 09:03:28 +0200

    initial

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compress: true,
+  optimizeFonts: true,
   swcMinify: true,
+  experimental: {
+    gzipSize: true,
+  }
 }
 
 module.exports = nextConfig
